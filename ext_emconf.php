4d5edb8445ddda25314bf9e386e04514b6783baa
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-22 14:19:52 +0000

    upload of version gap between 2.3.3 and 2.3.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dmmjobcontrol/trunk@22645 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "dmmjobcontrol"
 #
-# Auto generated 22-07-2009 16:16
+# Auto generated 22-07-2009 16:18
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Job database to show job offers on your site. Paginated lists, detailview, search function, apply form and RSS. It is possible to show the most recent jobs and/or the searchform on all pages. Easy to setup yet powerful and flexible.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '2.3.3',
+	'version' => '2.3.4',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:36:{s:9:"ChangeLog";s:4:"8381";s:12:"ext_icon.gif";s:4:"b6b0";s:17:"ext_localconf.php";s:4:"f7d9";s:14:"ext_tables.php";s:4:"37a5";s:14:"ext_tables.sql";s:4:"800e";s:28:"ext_typoscript_constants.txt";s:4:"ba97";s:24:"ext_typoscript_setup.txt";s:4:"ad53";s:15:"flexform_ds.xml";s:4:"1a61";s:31:"icon_tx_dmmjobcontrol_arrow.gif";s:4:"3d4b";s:34:"icon_tx_dmmjobcontrol_category.gif";s:4:"3073";s:33:"icon_tx_dmmjobcontrol_contact.gif";s:4:"3073";s:36:"icon_tx_dmmjobcontrol_discipline.gif";s:4:"3073";s:35:"icon_tx_dmmjobcontrol_education.gif";s:4:"3073";s:29:"icon_tx_dmmjobcontrol_job.gif";s:4:"5e8d";s:32:"icon_tx_dmmjobcontrol_job__h.gif";s:4:"9f08";s:33:"icon_tx_dmmjobcontrol_job__ht.gif";s:4:"4a78";s:32:"icon_tx_dmmjobcontrol_job__t.gif";s:4:"ea8f";s:32:"icon_tx_dmmjobcontrol_job__x.gif";s:4:"5e8d";s:32:"icon_tx_dmmjobcontrol_region.gif";s:4:"3073";s:32:"icon_tx_dmmjobcontrol_sector.gif";s:4:"3073";s:16:"locallang_db.xml";s:4:"fe2c";s:17:"locallang_tca.xml";s:4:"faff";s:7:"tca.php";s:4:"22df";s:34:"pi1/class.tx_dmmjobcontrol_pi1.php";s:4:"89e7";s:17:"pi1/locallang.xml";s:4:"3874";s:24:"pi1/static/editorcfg.txt";s:4:"08cd";s:14:"res/apply.tmpl";s:4:"b607";s:15:"res/detail.tmpl";s:4:"56bf";s:13:"res/list.tmpl";s:4:"f2bc";s:27:"res/markerArrayFunction.php";s:4:"cd62";s:12:"res/rss.tmpl";s:4:"9b41";s:15:"res/search.tmpl";s:4:"44a0";s:27:"res/searchArrayFunction.php";s:4:"740d";s:14:"doc/manual.sxw";s:4:"dd83";s:19:"doc/wizard_form.dat";s:4:"5794";s:20:"doc/wizard_form.html";s:4:"def0";}',
+	'_md5_values_when_last_written' => 'a:36:{s:9:"ChangeLog";s:4:"0a8c";s:12:"ext_icon.gif";s:4:"b6b0";s:17:"ext_localconf.php";s:4:"f7d9";s:14:"ext_tables.php";s:4:"37a5";s:14:"ext_tables.sql";s:4:"800e";s:28:"ext_typoscript_constants.txt";s:4:"ba97";s:24:"ext_typoscript_setup.txt";s:4:"ad53";s:15:"flexform_ds.xml";s:4:"1a61";s:31:"icon_tx_dmmjobcontrol_arrow.gif";s:4:"3d4b";s:34:"icon_tx_dmmjobcontrol_category.gif";s:4:"3073";s:33:"icon_tx_dmmjobcontrol_contact.gif";s:4:"3073";s:36:"icon_tx_dmmjobcontrol_discipline.gif";s:4:"3073";s:35:"icon_tx_dmmjobcontrol_education.gif";s:4:"3073";s:29:"icon_tx_dmmjobcontrol_job.gif";s:4:"5e8d";s:32:"icon_tx_dmmjobcontrol_job__h.gif";s:4:"9f08";s:33:"icon_tx_dmmjobcontrol_job__ht.gif";s:4:"4a78";s:32:"icon_tx_dmmjobcontrol_job__t.gif";s:4:"ea8f";s:32:"icon_tx_dmmjobcontrol_job__x.gif";s:4:"5e8d";s:32:"icon_tx_dmmjobcontrol_region.gif";s:4:"3073";s:32:"icon_tx_dmmjobcontrol_sector.gif";s:4:"3073";s:16:"locallang_db.xml";s:4:"fe2c";s:17:"locallang_tca.xml";s:4:"faff";s:7:"tca.php";s:4:"22df";s:34:"pi1/class.tx_dmmjobcontrol_pi1.php";s:4:"89e7";s:17:"pi1/locallang.xml";s:4:"3874";s:24:"pi1/static/editorcfg.txt";s:4:"08cd";s:14:"res/apply.tmpl";s:4:"b607";s:15:"res/detail.tmpl";s:4:"8e0e";s:13:"res/list.tmpl";s:4:"7e3d";s:27:"res/markerArrayFunction.php";s:4:"cd62";s:12:"res/rss.tmpl";s:4:"9b41";s:15:"res/search.tmpl";s:4:"44a0";s:27:"res/searchArrayFunction.php";s:4:"740d";s:14:"doc/manual.sxw";s:4:"1d9c";s:19:"doc/wizard_form.dat";s:4:"5794";s:20:"doc/wizard_form.html";s:4:"def0";}',
 );
 
 ?>
