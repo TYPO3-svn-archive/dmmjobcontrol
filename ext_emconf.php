b026e7560b8451d18a11a6b176d8957400fa465a
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-22 12:54:29 +0000

    import of TER version 1.15.5
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dmmjobcontrol/trunk@22625 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "dmmjobcontrol"
+#
+# Auto generated 22-07-2009 14:49
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'JobControl',
+	'description' => 'Job database to show (paginated) lists with job offers on your site, with a detailview, search function, apply form and RSS feeds. It is possible to show the most recent jobs and/or the searchform on all pages. Easy to setup yet powerful and flexible.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.15.5',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Kevin Renskers [DMM Development]',
+	'author_email' => 'kevin@dauphin-mm.nl',
+	'author_company' => 'DMM Development',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:35:{s:9:"ChangeLog";s:4:"f44c";s:12:"ext_icon.gif";s:4:"b6b0";s:17:"ext_localconf.php";s:4:"c910";s:14:"ext_tables.php";s:4:"37a5";s:14:"ext_tables.sql";s:4:"1ad3";s:28:"ext_typoscript_constants.txt";s:4:"a1b3";s:24:"ext_typoscript_setup.txt";s:4:"4593";s:15:"flexform_ds.xml";s:4:"1a61";s:31:"icon_tx_dmmjobcontrol_arrow.gif";s:4:"3d4b";s:34:"icon_tx_dmmjobcontrol_category.gif";s:4:"3073";s:33:"icon_tx_dmmjobcontrol_contact.gif";s:4:"3073";s:36:"icon_tx_dmmjobcontrol_discipline.gif";s:4:"3073";s:35:"icon_tx_dmmjobcontrol_education.gif";s:4:"3073";s:29:"icon_tx_dmmjobcontrol_job.gif";s:4:"5e8d";s:32:"icon_tx_dmmjobcontrol_job__h.gif";s:4:"9f08";s:33:"icon_tx_dmmjobcontrol_job__ht.gif";s:4:"4a78";s:32:"icon_tx_dmmjobcontrol_job__t.gif";s:4:"ea8f";s:32:"icon_tx_dmmjobcontrol_job__x.gif";s:4:"5e8d";s:32:"icon_tx_dmmjobcontrol_region.gif";s:4:"3073";s:32:"icon_tx_dmmjobcontrol_sector.gif";s:4:"3073";s:16:"locallang_db.xml";s:4:"0c65";s:17:"locallang_tca.xml";s:4:"1d61";s:7:"tca.php";s:4:"22df";s:34:"pi1/class.tx_dmmjobcontrol_pi1.php";s:4:"9369";s:17:"pi1/locallang.xml";s:4:"05b8";s:24:"pi1/static/editorcfg.txt";s:4:"08cd";s:14:"res/apply.tmpl";s:4:"19b6";s:15:"res/detail.tmpl";s:4:"56bf";s:13:"res/list.tmpl";s:4:"f2bc";s:27:"res/markerArrayFunction.php";s:4:"9e0e";s:12:"res/rss.tmpl";s:4:"9b41";s:15:"res/search.tmpl";s:4:"13e5";s:14:"doc/manual.sxw";s:4:"125f";s:19:"doc/wizard_form.dat";s:4:"5794";s:20:"doc/wizard_form.html";s:4:"def0";}',
+);
+
+?>
